d95f8684898951a28f7f81667ec7cf9f3c28a500
wupeng <591726269@qq.com>
2019-11-12 14:59:02 +0800

    33

--- src/main/java/com/wzp/community/model/User.java
+++ src/main/java/com/wzp/community/model/User.java
@@ -1,9 +1,12 @@
 package com.wzp.community.model;
 
+import lombok.Data;
+
 /**
  * @author wzp
  * @date 2019-10-30 11:39
  */
+@Data
 public class User {
     private Integer id;
     private String name;
@@ -11,64 +14,6 @@ public class User {
     private String token;
     private Long gmtCreate;
     private Long gmtModified;
+    private String avatarUrl;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccount_id() {
-        return account_id;
-    }
-
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Long gmtmodified) {
-        this.gmtModified = gmtModified;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", account_id='" + account_id + '\'' +
-                ", token='" + token + '\'' +
-                ", gmtCreate=" + gmtCreate +
-                ", gmtmodified=" + gmtModified +
-                '}';
-    }
 }
